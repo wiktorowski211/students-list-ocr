b12a91c3d8be00d08198daa4c0c9eb4bdb77d813
Marcin Wiktorowski <marcin.wiktorowski@appunite.com>
2020-08-01 18:03:16 +0200

    Removed deleting directory

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import shutil
 import sys
 
 import cv2
@@ -10,20 +8,7 @@ import mark_text
 import recognize_indexes
 
 
-def create_dir_if_not_exists(directory):
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-
-
-def remove_dir_if_exists(directory):
-    if os.path.exists(directory):
-        shutil.rmtree(directory)
-
-
 def process_images(input_data_dir, output_data_dir, n_files):
-    remove_dir_if_exists(output_data_dir)
-    create_dir_if_not_exists(output_data_dir)
-
     for file_number in range(0, n_files):
         input_file_path = f'{input_data_dir}/{file_number}.png'
         output_indexes_file_path = f'{output_data_dir}/{file_number}-indeksy.txt'
